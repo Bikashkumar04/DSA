06b5619f003e75ffe3307350391613da4fc70f75
Bikashkumar <bikashbikash756@gmail.com>
2024-12-11 22:19:00 +0530

    first commit

---
+++ src/Recursion/StringManipulation.java
@@ -0,0 +1,16 @@
+package Recursion;
+import java.util.*;
+import java.lang.String;
+public class String {
+
+    static String removeOccurence(String s,int idx){
+        String ans ="";
+
+    }
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+
+        String s =sc.nextLine();
+
+    }
+}
---
+++ src/StringBuilder/Toggle.java
@@ -0,0 +1,2 @@
+package StringBuilder;public class Toggle {
+}
